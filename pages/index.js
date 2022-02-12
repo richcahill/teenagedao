ee77f67899c179c24b5052bf0893b6afc17bda3b
Rich Cahill <4175591+richcahill@users.noreply.github.com>
2022-02-12 22:14:07 +0000

    add a blinking questionmark as i remember how to use react

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
 import Head from 'next/head';
 import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
 
 export default function Home() {
+
+  useEffect(() => {
+
+    const intervalId = setInterval(() => {
+      setQuestion(!question)
+    }, 800);
+
+    return () => clearInterval(intervalId);
+  },);
+
+  const [question, setQuestion] = useState("");
+
   return (
     <>
       <div className='h-screen bg-zinc-100'>
@@ -33,7 +46,7 @@ export default function Home() {
           </nav>
           <div className="call-to-action container m-auto text-white relative font-light flex flex-col justify-end flex-1 py-16">
             <p className="uppercase text-xl mb-4">imagine</p>
-            <p className="text-8xl tracking-tight max-w-4xl">finally, a beautiful hardware wallet.</p>
+            <p className="text-8xl tracking-tight max-w-4xl">finally, a beautiful hardware wallet{question?"?":""}</p>
           </div>
         </main>
         
