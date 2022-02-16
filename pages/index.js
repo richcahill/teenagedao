38a12f7ead7d0faecb1eaa9bf0181201e421c933
Reid Williams <reid@computable.io>
2022-02-15 21:00:38 -0800

    connect with metamask and get signing address

--- pages/index.js
+++ pages/index.js
@@ -7,24 +7,46 @@ import CallToAction from '../components/CallToAction';
 import Footer from '../components/Footer';
 import Hero from '../components/Hero';
 import Sign from '../components/Sign';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { ethers } from 'ethers';
 
 export default function Home() {
-  const [isSigning, setIsSigning] = useState(true);
+  const [isSigning, setIsSigning] = useState(false);
+  const [signingAddress, setSigningAddress] = useState(undefined);
 
-  let openClose = () => {
-    setIsSigning(!isSigning);
-  };
+  let web3Provider, signer = undefined;
+
+  let closeSigningModal = () => {
+    setIsSigning(false);
+  }
+
+  let startSigning = async () => {
+    await web3Provider.send("eth_requestAccounts", []);
+
+    signer = web3Provider.getSigner();
+    const address = await signer.getAddress();
+    setSigningAddress(address);
+
+    console.log(`Connected as ${address}`);
+
+    setIsSigning(true);
+  }
+
+  useEffect(() => {
+    // Do this here as window is not available elsewhere b/c of SSR
+    web3Provider = window.ethereum ? new ethers.providers.Web3Provider(window.ethereum) 
+      : undefined;
+  });
 
   return (
     <div className='h-screen bg-zinc-100 overflow-y-scroll'>
-      {isSigning && <Sign close={openClose} />}
+      {isSigning && <Sign close={closeSigningModal} />}
       <Hero />
       <CallToAction />
       <WhatIf />
       <Why />
       <How />
-      <Signatures openSigningModal={openClose} />
+      <Signatures openSigningModal={startSigning} />
       <LFB />
       <Footer />
     </div>
