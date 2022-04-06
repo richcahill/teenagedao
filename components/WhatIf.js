16f8998207ebf42969399ee0e0228e2c60227d79
Rich Cahill <rich@rc3.me>
2022-04-06 11:47:50 +0100

    rm collaborators section

--- components/WhatIf.js
+++ components/WhatIf.js
@@ -1,22 +1,22 @@
 export default function WhatIf(props) {
   return (
-    <section className="pt-10 pb-16 px-4">
-      <div className="container mx-auto relative ">
-        <div className="relative ">
-          <img src="/img/te-products.png" alt="teenage engineering products" />
+    <section className='pt-10 pb-16 px-4'>
+      <div className='container mx-auto relative '>
+        <div className='relative '>
+          <img src='/img/te-products.png' alt='teenage engineering products' />
         </div>
-        <div className="mt-6 uppercase text-xl  text-center tracking-wide ">
+        <div className='mt-6 uppercase text-xl  text-center tracking-wide '>
           what if...
         </div>
-        <div className="text-center text-8xl lowercase font-light tracking-tight mt-6">
+        <div className='text-center text-8xl lowercase font-light tracking-tight mt-6'>
           we build it together.
         </div>
-        <div className="text-center text-lg lowercase font-light tracking-tight mt-12">
-          yes — with you,{" "}
+        <div className='text-center text-lg lowercase font-light tracking-tight mt-12'>
+          yes — with you,{' '}
           <a
-            href="https://teenage.engineering"
-            target="_blank"
-            className=" border-b border-black"
+            href='https://teenage.engineering'
+            target='_blank'
+            className=' border-b border-black'
           >
             teenage engineering
           </a>
@@ -26,7 +26,7 @@ export default function WhatIf(props) {
         {/* collaborators */}
         {/* todo, more collaborators */}
         {/* <div className="flex justify-center space-x-16 items-center h-20 mt-8"> */}
-        <div className="flex flex-wrap justify-center items-center space-x-16 mt-8">
+        {/* <div className="flex flex-wrap justify-center items-center space-x-16 mt-8">
           <img
             src="/img/collaborators/colab.png"
             alt="colab"
@@ -38,7 +38,7 @@ export default function WhatIf(props) {
             alt="colab"
             className="h-14"
           />
-        </div>
+        </div> */}
       </div>
     </section>
   );
