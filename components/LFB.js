c192497c3a797be0c96f43d5ec34d175da45430f
Rich Cahill <rich@rc3.me>
2022-04-06 11:56:36 +0100

    sort out svg

--- components/LFB.js
+++ components/LFB.js
@@ -3,38 +3,10 @@ export default function LFB(props) {
     <>
       <section>
         <div className='container mx-auto h-full relative'>
-          <h2 className=' font-light w-full overflow-hidden text-center'>
-            LFB
-          </h2>
-          <h3 className='text-xl lg:text-3xl absolute lowercase tracking-tight leading-5 '>
-            let's
-            <br />
-            f**king
-            <br />
-            build
-            <br />
-            <br />
-            <span className='opacity-20'>
-              this
-              <br />
-              together
-            </span>
-          </h3>
+          <img src='/img/LFB.svg' alt='lfb' className='w-full' />
         </div>
       </section>
-      <style jsx>{`
-        h2 {
-          font-size: 42vw;
-          line-height: 1;
-          -webkit-text-stroke: 2px black;
-          -webkit-text-fill-color: transparent;
-        }
-
-        h3 {
-          top: 14%;
-          left: 14%;
-        }
-      `}</style>
+      <style jsx>{``}</style>
     </>
   );
 }
