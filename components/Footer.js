9c732626aa1b209a00f817c01a14a1c04f2e4af3
Rich Cahill <rich@rc3.me>
2022-11-01 13:39:14 +0000

    Update footer content and styling

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,22 @@
 export default function Footer(props) {
   return (
     <section className='mt-20 bg-te-black'>
-      <footer className='container mx-auto py-8 px-4 text-white font-light'>
-        footer
+      <footer className='container mx-auto py-8 px-4 text-white font-light flex justify-between'>
+        <h1 className='text-3xl font-light tracking-tight mx leading-8 text-white'>
+          <p className='opacity-70'>teenage</p>
+          <p className='opacity-100 relative z-20'>dao</p>
+        </h1>
+
+        <h1 className='text-3xl font-light tracking-tight mx leading-8 text-white'>
+          <a
+            className=''
+            href='https://twitter.com/teenageDAO'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
+            twitter
+          </a>
+        </h1>
       </footer>
     </section>
   );
