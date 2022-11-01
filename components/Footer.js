56de99b021476da09001fd4a59f68558bcd7bcec
Rich Cahill <rich@rc3.me>
2022-11-01 13:43:43 +0000

    give the footer room to breathe at the bottom

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer(props) {
   return (
     <section className='mt-20 bg-te-black'>
-      <footer className='container mx-auto py-8 px-4 text-white font-light flex justify-between'>
+      <footer className='container mx-auto py-8 pb-64 px-4 text-white font-light flex justify-between'>
         <h1 className='text-3xl font-light tracking-tight mx leading-8 text-white'>
           <p className='opacity-70'>teenage</p>
           <p className='opacity-100 relative z-20'>dao</p>
