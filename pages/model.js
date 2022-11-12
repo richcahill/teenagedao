5e1a24195960d1f9d6319c7218cbfe293d624bbc
Rich Cahill <rich@rc3.me>
2022-11-12 22:06:09 +0000

    basics of importing a model

--- pages/model.js
+++ pages/model.js
@@ -1,43 +1,43 @@
 import { createRoot } from 'react-dom/client';
 import React, { useRef, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
+import { useLoader } from '@react-three/fiber';
+import { Canvas, useFrame, Suspense } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
-function Box(props) {
-  // This reference will give us direct access to the mesh
-  const mesh = useRef();
+function Model() {
+  const model = useRef();
   // Set up state for the hovered and active state
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-  useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
-  // Return view, these are regular three.js elements expressed in JSX
+  const gltf = useLoader(GLTFLoader, '/models/wallet2.gltf');
+
+  useFrame((state, delta) => (model.current.rotation.y += 0.01));
+
   return (
     <mesh
-      {...props}
-      ref={mesh}
-      scale={active ? 1.5 : 1}
+      ref={model}
       onClick={(event) => setActive(!active)}
       onPointerOver={(event) => setHover(true)}
       onPointerOut={(event) => setHover(false)}
+      scale={!active ? 1.2 : 1}
     >
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+      <primitive object={gltf.scene} />
     </mesh>
   );
 }
 
-export default function Model() {
+export default function Scene() {
   return (
     <div className='h-screen bg-zinc-100 overflow-y-scroll'>
       <Canvas>
         <ambientLight />
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[1.2, 0, 0]} />
+        {/* <pointLight position={[10, 10, 10]} /> */}
+        <Model />
       </Canvas>
     </div>
   );
 }
 
 if (process.window) {
-  createRoot(document.getElementById('root')).render(<Model></Model>);
+  createRoot(document.getElementById('root')).render(<Scene />);
 }
